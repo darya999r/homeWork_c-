59c6106fbbf3c9a75d9d4bc99fc5ce567513044d
Darya Nesterova <darya999r@mail.ru>
2022-08-23 16:59:37 +0300

    homework2 task3

---
+++ homework_2/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 15
+//Напишите прогармму, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+
+Console.Write("Enter number day: ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+
+if(numberDay<1 || numberDay>7)
+{
+    Console.WriteLine("Error");
+}
+if(numberDay>=1 && numberDay<6)
+{
+    Console.WriteLine("Not a day off!");
+}
+if(numberDay==6)
+{
+    Console.WriteLine("Day off!!!");
+}
+if(numberDay==7)
+{
+    Console.WriteLine("Day off!!!");
+}
