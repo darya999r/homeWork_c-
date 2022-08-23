4c9d21f54678b8210d61a6d019d2d4e33be8f7a5
Darya Nesterova <darya999r@mail.ru>
2022-08-23 15:18:33 +0300

    task3

---
+++ homework_1/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 6
+//Наришите прогармму, которая на вход принимает число и выдает,
+//является ли число четным (делится ли оно на 2 без остатка).
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2==0)
+{
+    Console.WriteLine(number + " - even number.");//чётное число
+}
+else
+{
+    Console.WriteLine(number + " - odd number.");//нечётное число
+}
