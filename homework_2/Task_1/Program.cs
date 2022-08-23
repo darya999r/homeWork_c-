4b97b535a61cdc6dc7c11a5618fa2cbd0e66f9d0
Darya Nesterova <darya999r@mail.ru>
2022-08-23 16:18:19 +0300

    homework2 task1

---
+++ homework_2/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10
+//Напишите прогармму, которая на входе принимает трехзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+Console.WriteLine("Random number: " + num);
+
+int num1 = num/10;
+int num2 = num1%10;
+
+Console.WriteLine(num2);
