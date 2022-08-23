22d1f2a84cfacfeecd48721a2491a45ecf7ced26
Darya Nesterova <darya999r@mail.ru>
2022-08-23 16:00:42 +0300

    task4

---
+++ homework_1/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8
+//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+int numberN = new Random().Next(1,10);
+Console.WriteLine("Random number: " + numberN);
+int number = 2;
+
+while(number<=numberN)
+{
+    Console.WriteLine(number);
+    number+=2;
+}
