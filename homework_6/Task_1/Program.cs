c742c8e5bf12fd6d07c3e8830e3bfaf03373d267
Darya Nesterova <darya999r@mail.ru>
2022-09-07 17:24:36 +0300

    homework6

---
+++ homework_6/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Write("Enter M: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int [] array = GetArray (length);
+Console.WriteLine("Numbers: " + String.Join(", ",array));
+PositivNum(array);
+
+int [] GetArray(int size)
+{
+    int[] res = new int [size];
+    for(int i=0; i<size; i++)
+    {
+        Console.Write("Enter number" + (i+1) + ": ");
+        res[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return res;
+}
+
+void PositivNum (int [] newArray)
+{
+    int count = 0;
+
+    for(int i=0; i<newArray.Length; i++)
+    {
+        if (newArray[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine("Count of positiv numbers:" + count);
+}
---
+++ homework_6/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 43: Напишите программу, которая найдёт 
+//точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//k1*x - y + b1 = 0
+//k2*x - y + b2 = 0
+
+
+Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+//по формуле Крамера:
+double x = -(b1*(-1)-b2*(-1))/(k1*(-1)-k2*(-1));
+double y = -(k1*b2-k2*b1)/(k1*(-1)-k2*(-1));
+
+Console.WriteLine("Point of intersection: " + "(" + x + "; " + y + ")");
+
