e896f5098c653a39cb250fb6834053ee4c65309d
Darya Nesterova <darya999r@mail.ru>
2022-09-16 09:50:41 +0300

    homework7

---
+++ homework_7/Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает 
+//позиции элемента в двумерном массиве, и возвращает значение 
+//этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+
+int rows = Convert.ToInt32(new Random().Next(0,10));
+int columns = Convert.ToInt32(new Random().Next(0,10));
+int [,] matrix = new int [rows,columns];
+
+for (int i=0; i<rows; i++)
+{
+    for (int j=0; j<columns; j++)
+    {
+        matrix [i,j] = new Random().Next(-10,10);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter number line: ");             //i
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number column: ");           //j
+int col = Convert.ToInt32(Console.ReadLine());
+
+if (rows<line || columns<col) 
+{
+    Console.Write("There is no such number in the array!");
+}
+else
+{
+    Console.Write("Array element found: " + matrix[line-1,col-1]);
+}
+
---
+++ homework_7/Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int rows = Convert.ToInt32(new Random().Next(0,10));
+int columns = Convert.ToInt32(new Random().Next(0,10));
+double [,] matrix = new double [rows,columns];
+
+for (int i=0; i<rows; i++)
+{
+    for (int j=0; j<columns; j++)
+    {
+        matrix [i,j] = new Random().Next(-10,10);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Average of numbers in column: ");
+for (int j=0; j<columns; j++)
+{
+    double sumNumColumn = 0;
+    for (int i=0; i<rows; i++)
+    {
+        sumNumColumn += matrix[i,j];
+    }
+    Console.Write($"{Math.Round(sumNumColumn/rows, 2)}" + "; ");
+}
+
