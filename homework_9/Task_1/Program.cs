989a3d146497b32bc54af8e66a44dd9b0757e7f4
Darya Nesterova <darya999r@mail.ru>
2022-09-19 15:26:31 +0300

    homework9

---
+++ homework_9/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все 
+//натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Write("Enter number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNaturalNumbers (int M, int N)
+{
+    if (M<=N) return $"{M} " + PrintNaturalNumbers(M+1,N);
+    else return string.Empty;
+}
+
+Console.WriteLine(PrintNaturalNumbers(M,N));
---
+++ homework_9/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write("Enter number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int count=M; count<=N; count+=1)
+{
+    sum+=count;
+}
+
+Console.WriteLine("Sum of numbers from M to N: " + sum);
+
